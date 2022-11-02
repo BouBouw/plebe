4b14f4373f1a65558dc9dea7153e505ab14d38cc
BouBou <69978359+BouBouw@users.noreply.github.com>
2022-11-02 22:20:22 +0100

    init 2

---
+++ events/client/ready.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'ready',
+	once: false,
+execute: async (client) => {
+    console.log('[!] ready');
+    }
+}
---
+++ index.js
@@ -0,0 +1,37 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+
+const config = require('./config.json');
+
+const client = new Client({
+    intents: [ 
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildPresences
+    ],
+    partials: [
+        Partials.Message,
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.Reaction,
+        Partials.GuildScheduledEvent,
+        Partials.User,
+        Partials.ThreadMember,
+    ]
+});
+
+client.commands = new Collection();
+client.slashCommands = new Collection();
+
+client.on('ready', async () => {
+    require('./handler')(client);
+
+    const ready = require('./events/client/ready.js');
+    await ready.exectue(client);
+})
+
+client.login(config.token)
